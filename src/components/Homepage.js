8da94ea679bef43db443b7aa768fc342ba015e67
DIAGNAL <tomy.m@diagnal.com>
2022-01-10 15:42:09 +0530

    homepage outline

---
+++ src/components/Homepage.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Homepage.styles.scss';
+
+const Homepage = () => {
+    return (
+        <div className='homepage'>
+            <div className='pagemenu'>
+<div className='menuitem'>
+    <div className='content'> 
+    <h1 className='title'>Kurtis</h1>
+    <span className='subtitle'>shop menu</span>
+    </div>
+</div>
+
+<div className='menuitem'>
+    <div className='content'> 
+    <h1 className='title'>Kurtis32</h1>
+    <span className='subtitle'>shop menu</span>
+    </div>
+</div>
+
+<div className='menuitem'>
+    <div className='content'> 
+    <h1 className='title'>Kurtis3</h1>
+    <span className='subtitle'>shop menu</span>
+    </div>
+</div>
+<div className='menuitem'>
+    <div className='content'> 
+    <h1 className='title'>Kurtis4</h1>
+    <span className='subtitle'>shop menu</span>
+    </div>
+</div>
+<div className='menuitem'>
+    <div className='content'> 
+    <h1 className='title'>Kurtis4</h1>
+    <span className='subtitle'>shop menu</span>
+    </div>
+</div>
+
+
+
+
+
+            </div>
+            
+        </div>
+    );
+}
+
+export default Homepage;
