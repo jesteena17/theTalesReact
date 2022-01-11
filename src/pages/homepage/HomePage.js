f03b358741abbd25b7e28679c56c6b6812a27cd3
ERT <jesteena>
2022-01-11 10:00:22 +0530

    ASD

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Homepage.styles.scss';
 import MainMenu from '../../components/mainmenu/MainMenu';
+import './Homepage.styles.scss';
+
 const HomePage = () => {
     return (
         <div className='homepage'>
