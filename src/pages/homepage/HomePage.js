fff5470549f075c707f9c1d0795e60a5af6b3b65
ERT <jesteena>
2022-01-11 10:03:50 +0530

    testing source trees

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './Homepage.styles.scss';
 import MainMenu from '../../components/mainmenu/MainMenu';
-import './Homepage.styles.scss';
-
 const HomePage = () => {
     return (
         <div className='homepage'>
