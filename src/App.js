8b3d9d8047ca346bf04bee223d2cadf1e1c6d394
jesteena17 <jesteenababy17@gmail.com>
2022-01-11 11:33:20 +0530

    added basic routing and pkg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 
 import './App.css';
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
 import HomePage from './pages/homepage/HomePage';
+
+ const BabyPage=()=>{
+  return <div>Baby page</div>
+}
+
 function App() {
   return (
     <div>
-    <HomePage />
+
+      <BrowserRouter>
+    
+      <Routes>
+<Route exact path="/" element={<HomePage/>}/>
+<Route  path="/babies" element={<BabyPage/>}/>
+
+      </Routes>
+      
+      </BrowserRouter>
+   
     </div>
   );
 }
