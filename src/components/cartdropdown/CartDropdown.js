9f60b848e03e070b11aa886692a710b8a6c00083
jesteena17 <jesteenababy17@gmail.com>
2022-01-17 12:07:11 +0530

    on clicking add to cart btn on product added item to cart dropdwn list using redux

--- src/components/cartdropdown/CartDropdown.js
+++ src/components/cartdropdown/CartDropdown.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import CartItem from '../cartitem/CartItem';
 import CustomButton from '../custombutton/CustomButton' ;
 import './cartdropdown.styles.scss';
 
-const CartDropdown = () => {
+
+
+const CartDropdown = ({cartItems}) => {
+    console.log(cartItems);
     return (
         <div className='cart-dropdown'>
-            <div className='cart-item'/>
+            <div className='cart-items'>
+            {
+            cartItems.map(cartItem => {
+       return  <CartItem key={cartItem.id} item={cartItem} />
+            })
+            }
+
+
+           </div>
 <CustomButton >Go To Checkout</CustomButton> 
         </div>
     );
 }
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+
+  });
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -3,7 +3,7 @@ import { addItemtoCart } from './cart.utils';
 
 const INITIAL_STATE={
     hidden:true,
-    cartItem:[]
+    cartItems:[]
 }
 
 const cartReducer=(state=INITIAL_STATE,action)=>{
@@ -20,7 +20,7 @@ const cartReducer=(state=INITIAL_STATE,action)=>{
 
                 // cartItem:[...state.cartItem,action.payload]
                 //change this to check if item alredy exist in cart based in utils
-                cartItem:addItemtoCart(state.cartItem,action.payload)
+                cartItems:addItemtoCart(state.cartItems,action.payload)
             };
         default:
             return state;
