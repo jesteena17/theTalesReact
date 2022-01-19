c8507c90166d1853149c590993eb7912f62d26d8
jesteena17 <jesteenababy17@gmail.com>
2022-01-19 12:30:28 +0530

    January 19 updates

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,20 @@
 import { combineReducers } from "redux";
+import { persistReducer  } from "redux-persist";
+import storage from "redux-persist/lib/storage";//to use localstorage
 import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user.reducer";
 
-export default combineReducers({
+const persistConfig={
+    key:'root',
+    storage,
+    whitelist:['cart']//for now we need to localy storage cart bcz user is being stored nd mnged by firebase
+}
+
+
+const rootReducer=combineReducers({
     user:userReducer,
     cart:cartReducer,
 })
+
+
+export default persistReducer(persistConfig,rootReducer )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { createStore,applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
+//to use session or localstorage by redux
+import { persistStore } from "redux-persist";
+
 //middelwares are always arrays
   const middlewares=[logger];
 
-  const store=createStore(rootReducer,applyMiddleware(...middlewares)); 
+  export const store=createStore(rootReducer,applyMiddleware(...middlewares)); 
 
-  export default store;
+ export const persistor=persistStore(store);
+  export default {store,persistor};
